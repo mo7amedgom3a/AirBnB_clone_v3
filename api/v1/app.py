a1c1012e6b5bb9ecd6ae02530287808f74e825fb
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-05-02 01:27:42 +0300

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 
 app = Flask(__name__)
@@ -14,4 +16,7 @@ def close_storage(exception):
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+
+    app.run(host, int(port), threaded=True)
