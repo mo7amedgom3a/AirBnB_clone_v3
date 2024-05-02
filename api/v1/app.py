8bb54623d8483d985d02da8d412d98bd0e9dcaca
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-05-03 01:21:37 +0300

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,12 +6,13 @@ from models import storage
 from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
 from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close_db(obj):
